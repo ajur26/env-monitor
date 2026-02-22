4f796f04d56e2bab27b5d0089086d4a68941ffe7
Andrzej Jur <andrzej.w.jur@gmail.com>
2026-02-22 11:11:12 +0100

    feat: enable CORS for frontend integration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'measurements',
+    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,3 +135,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
