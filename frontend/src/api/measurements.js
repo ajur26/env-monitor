153cfe6db2dc862340d963e4cfdd4e132f860f9b
Andrzej Jur <andrzej.w.jur@gmail.com>
2026-03-12 20:44:00 +0100

    feat(frontend): add alarms and recent measurements API

--- frontend/src/api/measurements.js
+++ frontend/src/api/measurements.js
@@ -1,18 +1,49 @@
 const API_URL = "http://127.0.0.1:8000/api";
 const API_TOKEN = "super-secret-token";
 
-export async function fetchLastestMeasurement() {
-    const res = await fetch((`${API_URL}/measurements/?page=1`))
-    if(!res.ok) throw new Error("Failed to detch measurements");
+
+export async function fetchLatestMeasurement() {
+    const res = await fetch(`${API_URL}/measurements/?page=1`);
+
+    if (!res.ok) throw new Error("Failed to fetch measurements");
+
     const data = await res.json();
     return data.results[0];
 }
 
+
 export async function fetchStats() {
     const res = await fetch(`${API_URL}/measurements/stats/`);
+
     if (!res.ok) throw new Error("Failed to fetch stats");
+
     return await res.json();
 }
+
+
+export async function fetchRecentMeasurements(period = "1h", point = null) {
+    let url = `${API_URL}/measurements/recent/?period=${period}`;
+
+    if (point) {
+        url += `&point=${point}`;
+    }
+
+    const res = await fetch(url);
+
+    if (!res.ok) throw new Error("Failed to fetch recent measurements");
+
+    return await res.json();
+}
+
+
+export async function fetchAlarms() {
+    const res = await fetch(`${API_URL}/measurements/alarms/`);
+
+    if (!res.ok) throw new Error("Failed to fetch alarms");
+
+    return await res.json();
+}
+
 
 export async function addMeasurement(payload) {
     const res = await fetch(`${API_URL}/measurements/`, {
