02dfedf285eb6f59fadbd26347009c214a671ab1
Andrzej Jur <andrzej.w.jur@gmail.com>
2026-03-22 16:42:53 +0100

    feat(models): extend Measurement model with pressure and CO data fields; add Alarm model for event tracking

--- backend/measurements/models.py
+++ backend/measurements/models.py
@@ -11,12 +11,22 @@ class Measurement(models.Model):
 
     temperature = models.FloatField()
     humidity = models.FloatField()
-    co = models.IntegerField(null=True, blank=True)
+    pressure = models.FloatField()  # hPa
+
+    co_voltage = models.FloatField(null=True, blank=True)
+    co_status = models.CharField(max_length=20, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.point} | {self.temperature}°C | {self.humidity}% | {self.co} ppm | {self.created_at}"
+        return (
+            f"{self.point} | "
+            f"{self.temperature}°C | "
+            f"{self.humidity}% | "
+            f"{self.pressure} hPa | "
+            f"{self.co_voltage} V ({self.co_status}) | "
+            f"{self.created_at}"
+        )
 
 
 class Alarm(models.Model):
@@ -29,7 +39,8 @@ class Alarm(models.Model):
 
     temperature = models.FloatField(null=True, blank=True)
     humidity = models.FloatField(null=True, blank=True)
-    co = models.IntegerField(null=True, blank=True)
+    co_voltage = models.FloatField(null=True, blank=True)
+    co_status = models.CharField(max_length=20, null=True, blank=True)
 
     message = models.CharField(max_length=255)
 
