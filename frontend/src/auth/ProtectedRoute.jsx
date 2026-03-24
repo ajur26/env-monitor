c5851bb2e261280c5e2df00c6be2655e4427438e
Andrzej Jur <andrzej.w.jur@gmail.com>
2026-03-24 18:21:20 +0100

    chore(frontend): remove old frontend for rewrite

--- frontend/src/auth/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { isLoggedIn } from "./auth";
-
-export default function ProtectedRoute() {
-  return isLoggedIn() ? <Outlet /> : <Navigate to="/login" replace />;
-}
